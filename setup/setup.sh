9877342459d0bcd20234ea67ea22caa92ff89137
masonakeen <98773753+masonakeen@users.noreply.github.com>
2022-01-31 14:36:54 -0500

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,1 +1,17 @@
+#/bash
+# a bash script to setup the environment for the project
 
+# install pip3 and virtualenv
+sudo apt-get install python3-pip -y
+
+# create the virutual environment in the project root
+pip3 install virtualenv
+virtualenv -p python3 nba_moneyline
+
+# activate the virtual environment
+source nba_moneyline/bin/activate
+
+# install packages you will need
+pip3 install -r setup/requirements.txt
+
+python3 -m ipykernel install — user — name nba_moneyline — display-name "Custom NBA ML Kernel"
